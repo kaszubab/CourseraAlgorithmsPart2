7329fd339bcb851ad5f2231749e217839631c8fb
alzack67 <bartoszkaszuba1@gmail.com>
2019-10-06 21:34:02 +0200

    Entry validation

--- SeamCarving/SeamCarver.java
+++ SeamCarving/SeamCarver.java
@@ -15,10 +15,21 @@ public class SeamCarver {
         this.picture = new Picture(picture);
     }
 
+    private void checkValue (int a) {
+        if (a <= 0 || a >= picture.width()) throw new IllegalArgumentException();
+    }
     private void checkNull(Object a) {
         if(a == null) throw new IllegalArgumentException();
     }
 
+    private void checkSeam (int [] seam , int range) {
+        if (seam.length != range) throw new IllegalArgumentException();
+        checkValue(seam[0]);
+        for (int i = 1; i < range; i++) {
+            checkValue(seam[i]);
+            if (Math.abs(seam[i-1]-seam[i]) > 1) throw new IllegalArgumentException();
+        }
+    }
     // current picture
     public Picture picture() {
         return picture;
@@ -36,7 +47,10 @@ public class SeamCarver {
 
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
-        if (x <= 0 || y <= 0 || x >= picture.width() || y>= picture.height()) throw  new IllegalArgumentException();
+        checkNull(x);
+        checkNull(y);
+        checkValue(x);
+        checkValue(y);
     }
 
     // sequence of indices for horizontal seam
@@ -48,11 +62,15 @@ public class SeamCarver {
     // remove horizontal seam from current picture
     public void removeHorizontalSeam(int[] seam) {
         checkNull(seam);
+        checkSeam(seam,picture.height());
+        if (picture.height() == 1) throw new IllegalArgumentException();
     }
 
     // remove vertical seam from current picture
     public void removeVerticalSeam(int[] seam) {
         checkNull(seam);
+        checkSeam(seam,picture.width());
+        if (picture.width() == 1) throw new IllegalArgumentException();
     }
 
     //  unit testing (optional)
