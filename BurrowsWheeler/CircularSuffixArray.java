39d36ad93dd384302502a03e9f1e7c8a611ef385
alzack67 <bartoszkaszuba1@gmail.com>
2020-02-07 22:51:00 +0100

    Move to front implemented

--- BurrowsWheeler/CircularSuffixArray.java
+++ BurrowsWheeler/CircularSuffixArray.java
@@ -6,16 +6,26 @@
 
 public class CircularSuffixArray {
 
+
+    String mainString;
     // circular suffix array of s
-    public CircularSuffixArray(String s)
+    public CircularSuffixArray(String s) {
+        this.mainString = s;
+    }
 
     // length of s
-    public int length()
+    public int length() {
+        return mainString.length();
+    }
 
     // returns index of ith sorted suffix
-    public int index(int i)
+    public int index(int i) {
+        return 1;
+    }
 
     // unit testing (required)
-    public static void main(String[] args)
+    public static void main(String[] args) {
+
+    }
 
 }
--- BurrowsWheeler/MoveToFront.java
+++ BurrowsWheeler/MoveToFront.java
@@ -1,15 +1,72 @@
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
 
+import java.util.LinkedList;
 
 public class MoveToFront {
 
     // apply move-to-front encoding, reading from standard input and writing to standard output
-    public static void encode()
+    public static void encode() {
+
+        LinkedList<Integer> characterSequence = new LinkedList<>();
+
+        for (int i = 0; i < 256; i++) {
+            characterSequence.add(i);
+        }
+
+        while (!BinaryStdIn.isEmpty()) {
+            char c = BinaryStdIn.readChar();
+            int asciiCode = (int) c;
+
+            BinaryStdOut.write(characterSequence.indexOf(asciiCode));
+
+            characterSequence.remove((Integer) asciiCode);
+            characterSequence.addFirst(asciiCode);
+
+        }
+
+        BinaryStdOut.close();
+
+    }
 
     // apply move-to-front decoding, reading from standard input and writing to standard output
-    public static void decode()
+    public static void decode() {
+
+        LinkedList<Integer> characterSequence = new LinkedList<>();
+
+        for (int i = 0; i < 256; i++) {
+            characterSequence.add(i);
+        }
+
+        while (!BinaryStdIn.isEmpty()) {
+            char c = BinaryStdIn.readChar();
+            int asciiCode = (int) c;
+
+            System.out.println(1 + "  " + c);
+
+            BinaryStdOut.write((char) (int) characterSequence.get(asciiCode));
+
+
+            int character = characterSequence.remove(asciiCode);
+            characterSequence.addFirst(character);
+
+        }
+
+        BinaryStdOut.close();
+
+
+    }
 
     // if args[0] is "-", apply move-to-front encoding
     // if args[0] is "+", apply move-to-front decoding
-    public static void main(String[] args)
+    public static void main(String[] args) {
+
+        if (args[0].equals("-")) {
+            encode();
+        }
+        else if (args[0].equals("+")) {
+            decode();
+        }
+    }
 
 }
