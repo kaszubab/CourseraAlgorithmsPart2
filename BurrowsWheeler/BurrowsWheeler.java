bc323790896c3988610798a80cbde2e857203a39
alzack67 <bartoszkaszuba1@gmail.com>
2020-02-10 13:53:36 +0100

    Implemented Burrows Transform

--- BurrowsWheeler/BurrowsWheeler.java
+++ BurrowsWheeler/BurrowsWheeler.java
@@ -4,11 +4,42 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class BurrowsWheeler {
 
     // apply Burrows-Wheeler transform,
     // reading from standard input and writing to standard output
     public static void transform() {
+
+        String s = BinaryStdIn.readString();
+
+        CircularSuffixArray suffArr = new CircularSuffixArray(s);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (suffArr.index(i) == 0) {
+                BinaryStdOut.write( (int) i);
+                break;
+            }
+
+        }
+
+        char [] resultString = new char[s.length()];
+
+        for (int i = 0; i < suffArr.length(); i++) {
+
+            int characterPosition = suffArr.index(i) -1;
+            if (characterPosition == -1) characterPosition = s.length() - 1;
+            resultString[i] = s.charAt(characterPosition);
+        }
+
+        BinaryStdOut.write(new String(resultString));
+
+        BinaryStdOut.close();
 
     }
 
@@ -16,11 +47,60 @@ public class BurrowsWheeler {
     // reading from standard input and writing to standard output
     public static void inverseTransform() {
 
+
+        int first = BinaryStdIn.readInt();
+        String s = BinaryStdIn.readString();
+
+        if (s.length() == 1) {
+            BinaryStdOut.write(s);
+            BinaryStdOut.close();
+            return;
+        }
+
+        char [] sortedS = new char[s.length()];
+
+        int [] pos = new int[256];
+        Arrays.fill(pos, 0);
+
+        for (char x : s.toCharArray()) pos[x]++;
+        for (int i = 1; i < pos.length; i++) pos[i] = pos[i-1] + pos[i];
+        for (int i = 0; i < s.length(); i++) sortedS[pos[s.charAt(i)]-- - 1] = s.charAt(i);
+
+        int [] next = new int[s.length()];
+        HashMap<Character, Integer> seenRepetitions = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            int repetition = seenRepetitions.computeIfAbsent(sortedS[i], k -> 0);
+            int location = s.indexOf(sortedS[i], repetition);
+            if (location == i)  location++;
+            next[i] = s.indexOf(sortedS[i], location);
+            seenRepetitions.put(sortedS[i], s.indexOf(sortedS[i], location) + 1);
+        }
+
+        int tmp = first;
+        StringBuilder resultS = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            resultS.append(sortedS[tmp]);
+            tmp = next[tmp];
+        }
+
+
+        BinaryStdOut.write(resultS.toString());
+        BinaryStdOut.close();
     }
 
     // if args[0] is "-", apply Burrows-Wheeler transform
     // if args[0] is "+", apply Burrows-Wheeler inverse transform
     public static void main(String[] args) {
+
+        if (args[0].equals("-")) {
+            transform();
+        }
+        else if (args[0].equals("+")) {
+            inverseTransform();
+        }
+
 
     }
 
--- BurrowsWheeler/CircularSuffixArray.java
+++ BurrowsWheeler/CircularSuffixArray.java
@@ -4,105 +4,98 @@
  *  Description:
  **************************************************************************** */
 
-import java.util.ArrayList;
-
 public class CircularSuffixArray {
 
 
-    char [] mainString;
-    ArrayList<Integer> circularSuffixesOrder;
+    private String mainString;
+    private int [] circularSuffixesOrder;
 
     // circular suffix array of s
     public CircularSuffixArray(String s) {
 
         if (s == null) throw new IllegalArgumentException();
 
-        this.mainString = new char[s.length()];
+        this.mainString = s;
+
+        this.circularSuffixesOrder = new int[s.length()];
 
         for (int i = 0; i < s.length(); i++) {
-            mainString[i] = s.charAt(i);
+            circularSuffixesOrder[i] = i;
         }
 
-        this.circularSuffixesOrder = new ArrayList<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            circularSuffixesOrder.add(i);
-        }
-
-        charQuickSort(0, s.length() - 1);
-
-        ArrayList<Integer> tmp = new ArrayList<>();
-        for (int i = 0; i < s.length(); i++) {
-            tmp.add(0);
-        }
-
-        for (int i = 0; i < s.length(); i++) {
-            tmp.set(this.circularSuffixesOrder.get(i), i);
-        }
-
-        this.circularSuffixesOrder = tmp;
+        sort(0, s.length()-1,0);
 
     }
 
     private int compare(int suffixIndex1, int suffixIndex2) {
-        for (int i = 0; i < this.length(); i++) {
-            if (this.mainString[ (suffixIndex1+i) % this.length()] < this.mainString[ (suffixIndex2+i) % this.length()]) {
+        for (int i = 0; i < this.mainString.length(); i++) {
+            if (this.mainString.charAt((suffixIndex1+i) % this.mainString.length()) < this.mainString.charAt((suffixIndex2+i) % this.mainString.length())) {
                 return -1;
             }
-            else if (this.mainString[(suffixIndex1+i) % this.length()] > this.mainString[(suffixIndex2+i) % this.length()]) {
+            else if (this.mainString.charAt((suffixIndex1+i) % this.length()) > this.mainString.charAt((suffixIndex2+i) % this.mainString.length())) {
                 return 1;
             }
         }
         return 0;
     }
 
-    private int partition(int left, int right) {
-        int pivot = this.circularSuffixesOrder.get(right);
 
-        int i = left -1;
 
-        for (int j = left; j <= right; j++) {
-            if (compare(this.circularSuffixesOrder.get(j), pivot) < 0) {
-                i++;
-                int tmp = this.circularSuffixesOrder.get(i);
-                this.circularSuffixesOrder.set(i, this.circularSuffixesOrder.get(j));
-                this.circularSuffixesOrder.set(j, tmp);
+
+
+
+    private void sort(int lo, int hi, int d)
+        {
+            if (hi <= lo) return;
+            int lt = lo, gt = hi;
+            int v = -1;
+            if (d != this.mainString.length()) v = this.mainString.charAt((this.circularSuffixesOrder[lo] + d) % this.length());
+            int i = lo + 1;
+            while (i <= gt)
+            {
+                int t = this.mainString.charAt((this.circularSuffixesOrder[i] + d) % this.length());
+                if      (t < v) {
+                    int tmp = this.circularSuffixesOrder[lt];
+                    this.circularSuffixesOrder[lt] = this.circularSuffixesOrder[i];
+                    this.circularSuffixesOrder[i] = tmp;
+                    i++;
+                    lt++;
+                }
+                else if (t > v) {
+                    int tmp = this.circularSuffixesOrder[i];
+                    this.circularSuffixesOrder[i] = this.circularSuffixesOrder[gt];
+                    this.circularSuffixesOrder[gt] = tmp;
+                    gt--;
+                }
+                else            i++;
             }
-
+            // a[lo..lt-1] < v = a[lt..gt] < a[gt+1..hi]
+            sort(lo, lt-1, d);
+            if (v >= 0) sort(lt, gt, d+1);
+            sort(gt+1, hi, d);
         }
 
-        this.circularSuffixesOrder.set(right, this.circularSuffixesOrder.get(i+1));
-        this.circularSuffixesOrder.set(i + 1, pivot);
-
-        return i+1;
-    }
-
-    private void charQuickSort(int left, int right) {
-        if (left < right) {
-            int pivot = partition(left, right);
-            charQuickSort(left, pivot-1);
-            charQuickSort(pivot+1, right);
-        }
-
-    }
 
     // length of s
     public int length() {
-        return mainString.length;
+        return mainString.length();
     }
 
     // returns index of ith sorted suffix
     public int index(int i) {
 
-        if (i < 0 && i >= this.length()) throw new IllegalArgumentException();
+        if (i < 0 || i >= this.length()) throw new IllegalArgumentException();
 
-        return this.circularSuffixesOrder.get(i);
+        return this.circularSuffixesOrder[i];
     }
 
     // unit testing (required)
     public static void main(String[] args) {
-        CircularSuffixArray a = new CircularSuffixArray("ABRACADABRA!");
-        System.out.println(a.circularSuffixesOrder); // check the result of algorithm with the example
+        CircularSuffixArray a =
+                new CircularSuffixArray("ABRACADABRA!");
+        for (int i = 0; i < a.circularSuffixesOrder.length; i++) {
+            System.out.print(a.circularSuffixesOrder[i] + " ");
+        }
     }
 
 }
